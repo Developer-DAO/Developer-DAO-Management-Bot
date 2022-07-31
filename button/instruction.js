d0739b907be871ad29a17d7569939b8d9ce361e8
myz1237 <myz1237@myz1237deMacBook-Pro.local>
2022-08-01 01:36:54 +0800

    bug fix

--- button/instruction.js
+++ button/instruction.js
@@ -47,7 +47,7 @@ module.exports = {
                     if (collected.size == 0){
                         return interaction.editReply({
                             content: "Sorry, time out. Please run this command again.",
-                            components: []
+                            components: [],
                         })
                     }else{
                         const btnInteraction = collected.first();
@@ -64,8 +64,9 @@ module.exports = {
                                 })
                             }else{
                                 if(messageFetch.first().hasThread){
-                                    if (messageFetch.first().thread.name.startsWith("Welcome")) return interaction.followUp({
-                                        content: `You have had a welcome thread here <#${messageFetch.first().id}>`
+                                    if (messageFetch.first().thread.name.startsWith("Welcome")) return interaction.editReply({
+                                        content: `You have had a welcome thread here <#${messageFetch.first().id}>`,
+                                        components: [],
                                     })
                                     thread = await interaction.channel.threads.create({
                                         name: `Welcome ${interaction.member.displayName}`,
@@ -82,12 +83,12 @@ module.exports = {
                             })
                             return interaction.editReply({
                                 content: `Your thread has been created <#${thread.id}>`,
-                                components: []
+                                components: [],
                             })
                         }else{
                             return interaction.editReply({
                                 content: "Welcome to join in Developer DAO!",
-                                components: []
+                                components: [],
                             })
                         }
                     }
